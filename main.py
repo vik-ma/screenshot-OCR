6d8d76842599efc05c9700ad654ae0ed28af4e4a
Viktor <vikt.mag@gmail.com>
2022-07-24 03:34:05 +0200

    Moved GUI into MainWindow class

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QDesktopWidget, QPushButton
+from PyQt5.QtGui import QFont
 
-def draw_gui():
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Screenshot OCR")
+        self.setGeometry(0, 0, 700, 400)
+
+        #Display window in the center of the screen
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
+
+        self._centralWidget = QWidget(self)
+        self.setCentralWidget(self._centralWidget)
+
+        self.create_buttons()
+
+    def create_buttons(self):
+        self.test_button = QPushButton(self)
+        self.test_button.setText("TEST")
+        self.test_button.setFont(QFont("arial", 20, QFont.Bold))
+        self.test_button.setGeometry(5, 350, 100, 40)
+        self.test_button.clicked.connect(self.test)
+
+    def test(self):
+        pass
+
+
+def main():
+    """Main function."""
     app = QApplication([])
-    #Create 700x400 resizable GUI
-    window = QWidget()
-    window.setWindowTitle("Screenshot OCR")
-    window.setGeometry(0, 0, 700, 400)
-
-    #Center the window
-    qtRectangle = window.frameGeometry()
-    centerPoint = QDesktopWidget().availableGeometry().center()
-    qtRectangle.moveCenter(centerPoint)
-    window.move(qtRectangle.topLeft())
-
-    test_label = QLabel("<h1>TEST LABEL</h1>", parent=window)
-    test_label.move(60, 30)
-
-    window.show()
-
+    mw = MainWindow()
+    mw.show()
     sys.exit(app.exec_())
 
-draw_gui()
+if __name__ == "__main__":
+    main()
