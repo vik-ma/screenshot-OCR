b174d9549732c07f75d3962fe86fd70d8b4ced44
Viktor <vikt.mag@gmail.com>
2022-07-24 07:18:11 +0200

    Added basic snippet screenshot functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QDesktopWidget, QPushButton
-from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QDesktopWidget, QPushButton, QSplashScreen, QRubberBand
+from PyQt5.QtGui import QFont, QPixmap, QColor, QWindow, QMouseEvent, QGuiApplication
+from PyQt5.QtCore import QPoint, Qt, QRect, QSize
 
 
 class MainWindow(QMainWindow):
@@ -20,15 +21,68 @@ class MainWindow(QMainWindow):
 
         self.create_buttons()
 
+        self.snippet = CreateSnippet()
+    
+    def new_snippet(self):
+        self.snippet.show()
+
     def create_buttons(self):
         self.test_button = QPushButton(self)
         self.test_button.setText("TEST")
         self.test_button.setFont(QFont("arial", 20, QFont.Bold))
         self.test_button.setGeometry(5, 350, 100, 40)
-        self.test_button.clicked.connect(self.test)
+        self.test_button.clicked.connect(self.new_snippet)
 
     def test(self):
         pass
+class CreateSnippet(QSplashScreen):
+    def __init__(self):
+        super().__init__()
+
+        self.origin = QPoint(0,0)
+        self.end = QPoint(0,0)
+
+        self.rubberband = QRubberBand(QRubberBand.Rectangle, self)
+
+        self.dim_screen()
+
+    def dim_screen(self):
+        #Only primary screen
+        screen_geometry = QGuiApplication.primaryScreen().geometry()
+
+        screen_pixelmap = QPixmap(screen_geometry.width(), screen_geometry.height())
+        screen_pixelmap.fill(QColor(0,0,0))
+
+        self.setPixmap(screen_pixelmap)
+        
+        self.setWindowState(Qt.WindowState.WindowFullScreen)
+        self.setWindowOpacity(0.4)
+        
+    def mousePressEvent(self, event):
+        """Show rectangle at mouse position when left-clicked"""
+        if event.button() == Qt.LeftButton:
+            self.origin = event.pos()
+
+            self.rubberband.setGeometry(QRect(self.origin, QSize()))
+            self.rubberband.show()
+
+    def mouseMoveEvent(self, event):
+        """Resize rectangle as we move mouse, after left-clicked."""
+        self.rubberband.setGeometry(QRect(self.origin, event.pos()).normalized())
+
+    def mouseReleaseEvent(self, event):
+        """Upon mouse released, ask the main desktop's QScreen to capture screen on defined area."""
+        if event.button() == Qt.LeftButton:
+            self.end = event.pos()
+
+            self.rubberband.hide()
+            self.hide()
+
+            #Only primary screen
+            screen = QGuiApplication.primaryScreen()
+            selected_pixel_map = screen.grabWindow(0, self.origin.x(), self.origin.y(), self.end.x()-self.origin.x(), self.end.y()-self.origin.y())
+            selected_pixel_map.save("test.png", "png")
+
 
 
 def main():
